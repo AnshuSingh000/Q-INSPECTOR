531e424e0d99df992bc082372f1a833525d5d648
AnshuSingh000 <anshus.bsc23@rvu.edu.in>
2026-04-13 17:09:27 +0530

    Refactored optimizer to rebuild clean optimized circuits

--- qinspector/optimizer.py
+++ qinspector/optimizer.py
@@ -1,8 +1,9 @@
+from qiskit import QuantumCircuit
 from qinspector.utils import get_gate_name, get_qubit_indices, same_gate_and_qubits
 
 
 def optimize_circuit(circuit):
-    new_data = []
+    optimized_circuit = QuantumCircuit(circuit.num_qubits, circuit.num_clbits)
     applied_rules = []
     i = 0
     data = circuit.data
@@ -23,8 +24,12 @@ def optimize_circuit(circuit):
                 i += 2
                 continue
 
-        new_data.append(data[i])
+        current_inst = data[i]
+        optimized_circuit.append(
+            current_inst.operation,
+            current_inst.qubits,
+            current_inst.clbits
+        )
         i += 1
 
-    circuit.data = new_data
-    return circuit, applied_rules
+    return optimized_circuit, applied_rules
